0dff5f25705c38e5fd5195e86433fd8bd6ff2db6
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-12 19:57:18 -0500

    Fix rubocop errors

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :category_expenses, dependent: :destroy
-    has_many :categories, through: :category_expenses
+  belongs_to :author, class_name: 'User'
+  has_many :category_expenses, dependent: :destroy
+  has_many :categories, through: :category_expenses
 end
