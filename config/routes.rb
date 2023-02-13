00b320c50ad0ca8a9682995aa817ae63d142fa1b
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-13 08:04:44 -0500

    Configure nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :categories, except: [:update, :edit]
+  resources :categories, except: [:update, :edit] do
+    resources :expenses, only: [:new, :create]
+  end
 
   root "home#index"
 end
