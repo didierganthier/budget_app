7935e432924f08b25712ad12db3e9dd9373aff80
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-13 08:01:10 -0500

    Add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :category_expenses, dependent: :destroy
   has_many :expenses, through: :category_expenses
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
